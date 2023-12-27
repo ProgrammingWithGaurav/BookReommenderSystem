1c37a96a38b37508eaca1c24d68413555942b306
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2023-12-27 11:25:14 +0000

    adding post request

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import joblib
+from fastapi import FastAPI, Form
+from typing import Annotated
+import numpy as np
 
 app = FastAPI()
 popular_df = joblib.load('popular.joblib')
+pt = joblib.load('pt.joblib')
+books = joblib.load('books.joblib')
+similarity_scores = joblib.load('similarity_scores.joblib')
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
@@ -23,5 +29,22 @@ async def read_item(request: Request):
 
 
 @app.get("/recommend", response_class=HTMLResponse)
-async def recommend(request: Request):
+async def recommend_ui(request: Request):
     return templates.TemplateResponse("recommend.html", {"request": request})
+
+@app.post("/recommend_books")
+async def recommend(request:Request):
+    form = await request.form()
+    formDict = dict(form)
+    user_input = formDict["user_input"]
+    # index fetch
+    index = np.where(pt.index==user_input)[0][0]
+    similar_items = sorted(list(enumerate(similarity_score[index])), key=lambda x:x[1], reverse=True)[1:6]
+    
+    data = []
+    # looping items
+    for i in similar_items:
+        temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+        item = temp_df.drop_duplicates('Book-Title')
+        data.append(item)
+    print(data)
